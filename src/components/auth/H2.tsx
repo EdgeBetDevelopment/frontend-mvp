e6e2d314cad2901fd813bee14e837253ea1da7c0
Artem Zubkov <zubkov.artem46@gmail.com>
2025-08-15 23:23:29 +0300

    fixed responsive issues at auth screens

--- src/components/auth/H2.tsx
+++ src/components/auth/H2.tsx
@@ -1,5 +1,9 @@
 const H2: React.FC<{ text: string }> = ({ text }) => {
-  return <h2 className="text-center text-[40px] font-normal">{text}</h2>;
+  return (
+    <h2 className="text-center text-[32px] font-normal sm:text-[40px]">
+      {text}
+    </h2>
+  );
 };
 
 export default H2;
--- src/pages-components/auth/SignUpPage.tsx
+++ src/pages-components/auth/SignUpPage.tsx
@@ -16,7 +16,7 @@ const SignUpPage = () => {
 
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <div className="auth-form mt-20 mb-40 flex w-full max-w-[800px] flex-col items-center justify-center gap-6 rounded-3xl px-40 py-14">
+      <div className="auth-form mt-20 mb-40 flex w-full max-w-[800px] flex-col items-center justify-center gap-6 rounded-3xl px-6 py-14 sm:px-10 md:px-20 lg:px-40">
         <SignUpForm onSuccessSignUp={onSuccessSignUp} />
       </div>
     </div>
