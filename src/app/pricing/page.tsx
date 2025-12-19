901e19b4afb9523bdd853c2e0fa0293ed72a2c85
Artem Zubkov <zubkov.artem46@gmail.com>
2025-12-19 22:55:24 +0200

    added a new version of pricing page with corresponding components

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,16 +1,32 @@
+'use client'
+
 import React from 'react';
 
-import CTABlock from '@/components/CTABlock';
-import PricingBlock from '@/components/pricing/page';
+import Navigation from '@/components/Navigation';
+import { usePricingPlans } from '@/components/pricing/hooks/usePricingPlans';
+import {PricingHeader, PricingGrid, PricingDisclaimer, PricingFAQ} from "@/components/pricing/components";
+import Footer from "@/components/Footer";
 
 const PricingPage = () => {
-  return (
-    <div className="tl-container my-[90px] flex w-full flex-col items-center gap-[90px]">
-      <PricingBlock />
+    const { mappedPlans, loadingId, handleCheckout, isLoading } = usePricingPlans();
 
-      <CTABlock />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-background">
+            <Navigation />
+            <div className="container mx-auto px-6 py-24">
+                <PricingHeader />
+                <PricingGrid
+                    plans={mappedPlans}
+                    loadingId={loadingId}
+                    onCheckout={handleCheckout}
+                    isLoading={isLoading}
+                />
+                <PricingDisclaimer />
+                <PricingFAQ />
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default PricingPage;
